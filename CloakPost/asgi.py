bbb7555d9ecb2613927b3619e358eb75ecf6583f
Tausifislam01 <tausifislam2001@gmail.com>
2025-09-23 02:43:28 +0600

    fixed bugs

--- CloakPost/asgi.py
+++ CloakPost/asgi.py
@@ -1,15 +1,23 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import user_messages.routing  # websocket_urlpatterns
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CloakPost.settings")
 
-django_app = get_asgi_application()
+# Ensure Django apps are loaded before importing anything that touches models
+django.setup()
+
+# Create the HTTP application first (this also ensures setup is done)
+django_asgi_app = get_asgi_application()
+
+# Import routing only AFTER django.setup(), so consumers/models can import safely
+import user_messages.routing  # noqa: E402
 
 application = ProtocolTypeRouter({
-    "http": django_app,
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(user_messages.routing.websocket_urlpatterns)
     ),
