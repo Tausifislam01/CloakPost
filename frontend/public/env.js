f110a1b2ff10dfcd3e96b6987986352b326b8d73
Tausifislam01 <tausifislam2001@gmail.com>
2025-09-29 01:08:00 +0600

    fixing frontend env

--- frontend/public/env.js
+++ frontend/public/env.js
@@ -1,5 +1,5 @@
-// Replace these with your actual backend URLs
+// frontend/public/env.js
 window.__ENV = {
-  API_BASE: 'https://cloakpost.onrender.com',
-  WS_BASE: 'wss://cloakpost.onrender.com'
+  API_BASE: 'https://cloakpost-zmd1.onrender.com',
+  WS_BASE:  'wss://cloakpost-zmd1.onrender.com'
 }
